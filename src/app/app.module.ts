c65564a1c329397067075f6bca311e87cff5f5a5
LASfire <ledashev.andrew@yandex.ru>
2021-02-10 11:36:06 +0300

    JGL-4 Цвета и шрифты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavMenuComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [NgbActiveModal],
+  providers: [NgbModal, NgbActiveModal],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
