ea70042d26cf4804628989947749e00c64011bea
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-02-11 12:45:46 +0300

    JGL-8 слайдер

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { FeedbackSliderComponent } from './feedback-slider/feedback-slider.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [HomeRoutingModule],
+  declarations: [HomeComponent, FeedbackSliderComponent],
+  imports: [HomeRoutingModule, NgbCarouselModule, CommonModule],
 })
 export class HomeModule {}
