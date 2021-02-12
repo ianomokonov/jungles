90a7815c1f8cba6ccd2330e9b8ae1410c511c357
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-02-12 11:28:13 +0300

    JGL-10 убрал circular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { FooterComponent } from './footer/footer.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     FooterComponent,
-    RegisterComponent,
-    LoginComponent,
+    // RegisterComponent,
+    // LoginComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [NgbModal, NgbActiveModal],
