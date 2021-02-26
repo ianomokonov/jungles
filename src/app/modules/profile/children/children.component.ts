db744d529f098672118de223d2450d2eaac99bbc
LASfire <ledashev.andrew@yandex.ru>
2021-02-26 16:21:34 +0300

    JGL-12 Начало меню

--- src/app/modules/profile/children/children.component.ts
+++ src/app/modules/profile/children/children.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-children',
   templateUrl: './children.component.html',
-  styleUrls: ['./children.component.less']
+  styleUrls: ['./children.component.less'],
 })
 export class ChildrenComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
