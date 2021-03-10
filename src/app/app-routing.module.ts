5840c82860b0f3b5be3d2634494142feeba7a678
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-03-10 10:33:08 +0300

    JGL-30

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
       url: '/profile/children',
       style: {
         'background-image': 'url(./assets/images/fon_admin2.png)',
-        // 'background-size': '100% 100%',
+        'background-size': '100% auto',
         // 'background-position': 'center -200%',
         'background-repeat': 'no-repeat',
       },
