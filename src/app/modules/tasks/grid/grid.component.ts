50e9cae0963610c83b5df12992471e09b8713dbc
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-03-30 21:56:36 +0300

    адаптив

--- src/app/modules/tasks/grid/grid.component.ts
+++ src/app/modules/tasks/grid/grid.component.ts
@@ -8,12 +8,18 @@ import { Component } from '@angular/core';
 export class GridComponent {
   public tasks = [];
   private breakIndexes: number[] = [0, 3, 5, 8, 12, 14, 17];
+  private breakIndexesSm: number[] = [0, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18, 19];
 
   constructor() {
     this.tasks.length = 20;
   }
 
   public isBreakShown(index: number) {
-    return this.breakIndexes.indexOf(index % 19) > -1;
+    const width = window.innerWidth;
+    if (width > 767) {
+      return this.breakIndexes.indexOf(index % 19) > -1;
+    }
+
+    return this.breakIndexesSm.indexOf(index % 19) > -1;
   }
 }
