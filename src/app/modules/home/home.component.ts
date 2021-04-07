b4588ecd5a620f15ad008649465eefd4e409f580
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-04-07 12:16:14 +0300

    в

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { childAnswersKey, userTasksInfoKey } from 'src/app/constants';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less'],
 })
-export class HomeComponent {
-  constructor() {
-    sessionStorage.removeItem(childAnswersKey);
-    sessionStorage.removeItem(userTasksInfoKey);
-  }
-}
+export class HomeComponent {}
